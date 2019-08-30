e3a02b1cc029349c36b31a32af2d4d13fa758a2a
Saulo Paiva <saulo@webfatorial.com>
2019-08-29 21:59:30 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Graph of Ticket to Ride map routes using D3.js
+
+Source:
+
+![](https://static.tvtropes.org/pmwiki/pub/images/ticket_to_ride_map.jpg)
+
+Result:
+
+![](https://i.ibb.co/NxqXRss/graph.png)
---
+++ index.js
@@ -0,0 +1,64 @@
+import routes from './data.js'
+
+(function() {
+  var nodes = {};
+
+  routes.forEach(function(link) {
+    link.source = nodes[link.source] || (nodes[link.source] = {name: link.source});
+    link.target = nodes[link.target] || (nodes[link.target] = {name: link.target});
+  });
+
+  var width = 960,
+      height = 700;
+
+  var force = d3.layout.force()
+    .nodes(d3.values(nodes))
+    .links(routes)
+    .size([width, height])
+    .linkDistance(function (d) {
+      return d.size * 25;
+    })
+    .charge(-600)
+    .on("tick", tick)
+    .start();
+
+  var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+  var path = svg.append("g").selectAll("path")
+    .data(force.links())
+  .enter().append("path")
+    .attr("class", function(d) { return "link " + d.color; });
+
+  var circle = svg.append("g").selectAll("circle")
+    .data(force.nodes())
+  .enter().append("circle")
+    .attr("r", 6)
+    .call(force.drag);
+
+  var text = svg.append("g").selectAll("text")
+    .data(force.nodes())
+  .enter().append("text")
+    .attr("x", 8)
+    .attr("y", ".31em")
+    .text(function(d) { return d.name; });
+
+  function tick() {
+    path.attr("d", linkArc);
+    circle.attr("transform", transform);
+    text.attr("transform", transform);
+  }
+
+  function linkArc(d) {
+    var dx = d.target.x - d.source.x,
+        dy = d.target.y - d.source.y,
+        dr = Math.sqrt(dx * dx + dy * dy);
+        
+    return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
+  }
+
+  function transform(d) {
+    return "translate(" + d.x + "," + d.y + ")";
+  }
+})()
